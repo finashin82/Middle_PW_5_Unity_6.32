c177e060fbc2d4a952df9ec940d6646fd1329bee
finashin82 <finashin82@mail.ru>
2025-02-23 10:09:14 +0900

    Добавил метод Construct

--- Middle_PW_5_Unity_6.32/Assets/Scripts/PlayerDamage.cs
+++ Middle_PW_5_Unity_6.32/Assets/Scripts/PlayerDamage.cs
@@ -5,6 +5,11 @@ public class PlayerDamage : MonoBehaviour
 {
     [Inject] private PlayerSettings _playerSettings;
 
+    public void Construct(PlayerSettings playerSettings)
+    {
+        _playerSettings = playerSettings;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.TryGetComponent<EnemyHealth>(out var health))
