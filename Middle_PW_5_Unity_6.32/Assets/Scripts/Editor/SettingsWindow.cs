0019b03dbd988202dc1a3dc3ab1faa94d96c538f
finashin82 <finashin82@mail.ru>
2025-04-26 14:07:45 +0900

    Почти сделал

--- Middle_PW_5_Unity_6.32/Assets/Scripts/Editor/SettingsWindow.cs
+++ Middle_PW_5_Unity_6.32/Assets/Scripts/Editor/SettingsWindow.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -6,13 +7,13 @@ public class SettingsWindow : EditorWindow
 {
     private string[] settingsList;
 
-    private string[] fileNames;
-
     private int selectedIndex = 0;
 
     private string assetsPath = "Assets/ScriptableObject";
 
-    private List<ScriptableObject> scriptableObjects = new List<ScriptableObject>();
+    private ScriptableObject loadObject;
+
+    private Editor editor;
 
     [MenuItem("Window/Settings window")]
     public static void ShowWindow()
@@ -22,22 +23,58 @@ public class SettingsWindow : EditorWindow
 
     private void OnGUI()
     {
-        // Получаем все .asset в указанной папке
-        settingsList = AssetDatabase.FindAssets("t:ScriptableObject", new[] { assetsPath });
-
+        // Заголовок
         GUILayout.Label("Game Settings", EditorStyles.boldLabel);
 
+        // Горизонтальная полоса (разделитель)
         GUILayout.Space(5);
         GUILayout.Box("", GUILayout.ExpandWidth(true), GUILayout.Height(1));
         GUILayout.Space(5);
 
-        selectedIndex = EditorGUILayout.Popup("Select Setting:", selectedIndex, settingsList);
+        // Получаем все .asset в указанной папке (в виде идентификаторов)
+        settingsList = AssetDatabase.FindAssets("t:ScriptableObject", new[] { assetsPath });
+
+        // Массив для имен
+        var settingsName = new string[settingsList.Length];
+
+        // Заполняем массив именами
+        for (int i = 0; i < settingsList.Length; i++)
+        {
+            // Путь к файлу
+            var settingsPath = AssetDatabase.GUIDToAssetPath(settingsList[i]);
+
+            // Получаем имена файлов
+            var settingsPathName = Path.GetFileNameWithoutExtension(settingsPath);
+
+            // Помещаем имена в массив
+            settingsName[i] = Path.GetFileName(settingsPathName);
+        }
+        
+        // Список с именами файлов
+        selectedIndex = EditorGUILayout.Popup("Select Setting:", selectedIndex, settingsName);
 
         if (GUILayout.Button("Show Selected"))
         {
-            Debug.Log("Selected: " + settingsList[selectedIndex]);
-        }
+            // Получаем путь к выбранному из списка файлу
+            var gameSettingPath = AssetDatabase.GUIDToAssetPath(settingsList[selectedIndex]);
 
-        //ScriptableObject asset = AssetDatabase.LoadAssetAtPath<ScriptableObject>(assetsPath);
+            // Загружаем файл с настройками по известному пути
+            loadObject = AssetDatabase.LoadAssetAtPath<ScriptableObject>(gameSettingPath);
+
+            if (loadObject != null) 
+            {
+                editor = Editor.CreateEditor(loadObject);
+
+                editor.OnInspectorGUI();
+
+                Debug.Log("+");
+            }
+            else
+            {
+                Debug.LogError($"Файл не существует: {gameSettingPath}");
+            }
+
+            Debug.Log($"Имя файла: {settingsName[selectedIndex]}. Путь к этому файлу: {gameSettingPath}");
+        }
     }
 }
