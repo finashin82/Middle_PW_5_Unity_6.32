8d92669b9206508b168844e63bf07b5f6e6781b5
finashin82 <finashin82@mail.ru>
2025-04-28 20:36:02 +0900

    Middle_PW_11. Готово

---
+++ Middle_PW_5_Unity_6.32/Assets/ScriptableObject/TestObject.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TestObject : MonoBehaviour
+{
+    [SerializeField] private Settings1 scriptableObject;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Middle_PW_5_Unity_6.32/Assets/Scripts/Editor/SettingsWindow.cs
+++ Middle_PW_5_Unity_6.32/Assets/Scripts/Editor/SettingsWindow.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
 public class SettingsWindow : EditorWindow
 {
+    List<GameObject> gameObject;
+
     private string[] settingsList;
 
     private int selectedIndex = 0;
@@ -13,7 +16,9 @@ public class SettingsWindow : EditorWindow
 
     private ScriptableObject loadObject;
 
-    private Editor editor;
+    private bool isNull = false;
+
+    private bool isWork = false;
 
     [MenuItem("Window/Settings window")]
     public static void ShowWindow()
@@ -49,9 +54,11 @@ public class SettingsWindow : EditorWindow
             // Помещаем имена в массив
             settingsName[i] = Path.GetFileName(settingsPathName);
         }
-        
+
         // Список с именами файлов
         selectedIndex = EditorGUILayout.Popup("Select Setting:", selectedIndex, settingsName);
+
+        
 
         if (GUILayout.Button("Show Selected"))
         {
@@ -61,20 +68,91 @@ public class SettingsWindow : EditorWindow
             // Загружаем файл с настройками по известному пути
             loadObject = AssetDatabase.LoadAssetAtPath<ScriptableObject>(gameSettingPath);
 
-            if (loadObject != null) 
+            //Debug.Log($"loadObject.Name {loadObject.name}");
+
+           
+
+            if (loadObject != null)
             {
-                editor = Editor.CreateEditor(loadObject);
+                gameObject = FindScriptableObjectInComponent(loadObject);
 
-                editor.OnInspectorGUI();
-
-                Debug.Log("+");
+                if (gameObject.Count == 0) 
+                {
+                    isNull = true;
+                }
+                else
+                {
+                    isNull = false;
+                }
             }
             else
             {
                 Debug.LogError($"Файл не существует: {gameSettingPath}");
             }
-
-            Debug.Log($"Имя файла: {settingsName[selectedIndex]}. Путь к этому файлу: {gameSettingPath}");
         }
+
+        if (isNull)
+        {
+            GUILayout.Label($"Файл {loadObject} никто не использует", EditorStyles.boldLabel);
+        }
+
+        if (isWork)
+        {
+            foreach (var obj in gameObject)
+                GUILayout.Label($"Объект {obj.name} использует ScriptableObject {loadObject.name}");
+
+        }
+    }
+
+    /// <summary>
+    /// Находим объекты которые используют нужные нам ScriptableObject
+    /// </summary>
+    /// <param name="scriptableObject"></param>
+    /// <returns></returns>
+    private List<GameObject> FindScriptableObjectInComponent(ScriptableObject scriptableObject)
+    {
+        // Получаем все объекты на активной сцене с помощью FindObjectsByType
+        GameObject[] allObjects = FindObjectsByType<GameObject>(FindObjectsSortMode.None);
+
+        // Создаем список для объектов, которые будут содержать ссылки на ScriptableObject
+        List<GameObject> objectsUsingScriptableObject = new List<GameObject>();
+
+        foreach (var obj in allObjects)
+        {
+            // Получаем все компоненты объекта
+            Component[] components = obj.GetComponents<Component>();
+
+            foreach (var component in components)
+            {
+                if (component != null)
+                {
+                    // Собираем все поля компонента
+                    FieldInfo[] fields = component.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+                    foreach (var field in fields)
+                    {
+                        // Проверяем, есть ли поле ScriptableObject в компоненте
+                        if (field.FieldType.IsSubclassOf(typeof(ScriptableObject)))
+                        {
+                            // Берем значение поля этого экземпляра объекта
+                            ScriptableObject referencedScriptableObject = field.GetValue(component) as ScriptableObject;
+
+                            if (referencedScriptableObject == scriptableObject)
+                            {
+                                objectsUsingScriptableObject.Add(obj);
+
+                                //Debug.Log($"Объект {obj.name} использует ScriptableObject {scriptableObject.name} в компоненте {component.GetType().Name}");
+                                
+                                isWork = true;
+
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return objectsUsingScriptableObject;
     }
 }
