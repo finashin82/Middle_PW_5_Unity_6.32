36e8edb48c73902d08e1a773d23f4c3efee2572d
finashin82 <finashin82@mail.ru>
2025-06-30 09:48:50 +0900

    Делаю PW 13

---
+++ Middle_PW_5_Unity_6.32/Assets/Scripts/Photon/NetworkManager.cs
@@ -0,0 +1,26 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        RoomOptions options = new RoomOptions
+        {
+            MaxPlayers = 4,
+            IsVisible = false
+        };
+        PhotonNetwork.JoinOrCreateRoom("test", options, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.CurrentRoom.PlayerCount);
+    }
+}
